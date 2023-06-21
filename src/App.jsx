32485f3d87a113786654b9f3da1a071c11736222
Ayush11010 <jain.68@iitj.ac.in>
2023-06-22 03:20:12 +0530

    Initial env setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/navbar'
+
+function App() {
+
+  return (
+    <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ... mx-12">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+export default function Navbar() {
+  return (
+    <div className= "italic-cursive-typing m-4 text-center flex space-x-2 justify-center text-5xl p-8">
+      <div className="text-[#4deeea]"> I</div>
+      <div className="text-[#bd46bb]"> A</div>
+      <div className="text-[#19d225]"> m</div>
+      <div className="text-[#4deeea]"> A</div>
+      <div className="text-[#4deeea]"> y</div>
+      <div className="text-[#4deeea]"> u</div>
+      <div className="text-[#4deeea]"> s</div>
+      <div className="text-[#4deeea]"> h</div>
+      <div className="text-[#4deeea]"> J</div>
+      <div className="text-[#4deeea]"> a</div>
+      <div className="text-[#4deeea]"> i</div>
+      <div className="text-[#4deeea]"> n</div>
+    </div>  
+  )
+}
